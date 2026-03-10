b22d872b709eec3448b040fcfd4933738e90ff43
Leo <sokolov.nikita01@list.ru>
2026-03-10 04:18:27 +0300

    menu + items

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -6,7 +6,7 @@ export default function Menu() {
           <div className="rounded-b-2xl w-250 font-[Comfortaa] mt-10 bg-[#ffffff] text-black">
             <div className="items-center text-center mb-10">
               <h1 className="m-10 text-2xl ">Меню</h1>
-              <h1 className="text-[14px] font-light">Напиток на основе эспрессо с добавлением горячей воды. <br />Пропорции могут варьироваться, но обычно используется<br /> соотношение 1:3 эспрессо к воде, в зависимости <br />от желаемой крепости напитка.</h1>
+              <h1 className="text-[14px] font-light">Описание меню</h1>
             </div>
             <div className="justify-center m-20 mb-10">
               <Link href="/menu/latte"><div className="">Латте</div></Link>
