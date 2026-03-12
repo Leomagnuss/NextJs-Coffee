82eed3593cfdb3ff3525f5479ad6016b02b3e4e8
Leo <sokolov.nikita01@list.ru>
2026-03-12 21:13:23 +0300

    modify menu

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -10,10 +10,10 @@ export default function Menu() {
           <h1 className="text-[16px] mt-10 font-light">Попробуйте классику мира кофе</h1>
         </div>
         <ul className="flex mb-20 pl-20 pr-20 justify-around ">
-          <div className="bg-gray-200 hover:bg-white duration-200 p-3 rounded-[10px]"><Link href="/menu/latte"><Image src="/LatteCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="rounded-2xl bg-cover">Латте</li></Link></div>
-          <div className="bg-gray-200 hover:bg-white duration-200 p-3 rounded-[10px]"><Link href="/menu/espresso"><Image src="/EspressoCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="">Эспрессо</li></Link></div>
-          <div className="bg-gray-200 hover:bg-white duration-200 p-3 rounded-[10px]"><Link href="/menu/cappuchino"><Image src="/CappuchinoCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="">Каппучино</li></Link></div>
-          <div className="bg-gray-200 hover:bg-white duration-200 p-3 rounded-[10px]"><Link href="/menu/americano"><Image src="/AmericanoCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="">Американо</li></Link></div>
+          <div className="bg-gray-200 hover:bg-gray-300 duration-200 p-3 rounded-[10px]"><Link href="/menu/latte"><Image src="/LatteCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="rounded-2xl bg-cover">Латте</li></Link></div>
+          <div className="bg-gray-200 hover:bg-gray-300 duration-200 p-3 rounded-[10px]"><Link href="/menu/espresso"><Image src="/EspressoCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="">Эспрессо</li></Link></div>
+          <div className="bg-gray-200 hover:bg-gray-300 duration-200 p-3 rounded-[10px]"><Link href="/menu/cappuchino"><Image src="/CappuchinoCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="">Каппучино</li></Link></div>
+          <div className="bg-gray-200 hover:bg-gray-300 duration-200 p-3 rounded-[10px]"><Link href="/menu/americano"><Image src="/AmericanoCard.jpg" className="rounded-lg mb-2" width={150} height={150} alt="" /><li className="">Американо</li></Link></div>
         </ul>
       </div>
     </div>
